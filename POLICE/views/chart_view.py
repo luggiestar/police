bf5bb8596edc87289742d351a522c5175620004b
ranboy12 <frmsonge@mzumbe.ac.tz>
2022-06-13 15:44:17 +0300

    new days1

--- POLICE/views/chart_view.py
+++ POLICE/views/chart_view.py
@@ -9,23 +9,22 @@ import plotly.express as px
 
 def chart_view(request):
     qs = Chart.objects.all()
-    try:
 
-        projects_data = [
-            {
-                'complaint': x.complaint.code,
-                'Start': x.start_date,
-                'Finish': x.finish_date,
-                'crime': x.crime.name
-            } for x in qs
-        ]
-        df = pd.DataFrame(projects_data)
-        fig = px.timeline(
-            df, x_start="Start", x_end="Finish", y="complaint", color="crime"
-        )
-        fig.update_yaxes(autorange="reversed")
-        gantt_plot = plot(fig, output_type="div")
-    except:
-        gantt_plot = None
+
+    projects_data = [
+        {
+            'complaint': x.complaint.code,
+            'Start': x.start_date,
+            'Finish': x.finish_date,
+            'crime': x.crime.name
+        } for x in qs
+    ]
+    df = pd.DataFrame(projects_data)
+    fig = px.timeline(
+        df, x_start="Start", x_end="Finish", y="complaint", color="crime"
+    )
+    fig.update_yaxes(autorange="reversed")
+    gantt_plot = plot(fig, output_type="div")
+
     context = {'plot_div': gantt_plot, 'chart': qs}
     return render(request, 'chart/index.html', context)
