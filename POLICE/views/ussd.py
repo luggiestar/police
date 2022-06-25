ccb50acae48c96aa6ddb9a5544c676c608a45063
ranboy12 <frmsonge@mzumbe.ac.tz>
2022-06-25 18:43:10 +0300

    re29

--- POLICE/views/ussd.py
+++ POLICE/views/ussd.py
@@ -29,25 +29,41 @@ def ussd(request):
 
             get_code = Complainant.objects.filter(code=text).count()
             # get_complaint2 = Complainant.objects.filter(code=text).first()
-            get_number=get_code
+            get_number = get_code
             if get_number == 1:
                 get_complaint = Complainant.objects.filter(code=text).first()
-                case1=Case.objects.filter(complainant=get_complaint).order_by('-id')[0]
-                case2=Case.objects.filter(complainant=get_complaint).order_by('-id')[1]
-                case3=Case.objects.filter(complainant=get_complaint).order_by('-id')[2]
-                case4=Case.objects.filter(complainant=get_complaint).order_by('-id')[3]
-                code = get_complaint.code
+                try:
+                    case1 = Case.objects.filter(complainant=get_complaint).order_by('-id')[0]
+                    c1 = case1.rb
+                except:
+                    c1 = None
+                try:
+                    case2 = Case.objects.filter(complainant=get_complaint).order_by('-id')[1]
+                    c2 = case2.rb
+                except:
+                    c2 = None
+                try:
 
-                response = "END Karibu {0}: {1} {2} \n Majarada lako  ni\n{3} ".format(get_complaint.code,
-                                                                                get_complaint.user.first_name,
-                                                                                get_complaint.user.last_name,case1.rb,case2.rb,case3.rb,case4.rb)
+                    case3 = Case.objects.filter(complainant=get_complaint).order_by('-id')[2]
+                    c3 = case3.rb
+                except:
+                    c3 = None
+                try:
+
+                    case4 = Case.objects.filter(complainant=get_complaint).order_by('-id')[3]
+                    c4 = case4.rb
+                except:
+                    c4 = None
+
+                response = "END Karibu {0}: {1} {2} \n Majarada yako  ni\n{3}\n{4}\n{5}\n{6} ".format(get_complaint.code,
+                                                                                       get_complaint.user.first_name,
+                                                                                       get_complaint.user.last_name, c1,
+                                                                                       c2, c3, c4)
 
             # if get_number == 1 and text == "{0}*1".format(get_complaint2.code):
             #     response = "END umechagua moja {0}".format(get_complaint2.code)
-                # else:
-                #     response = "END umechagua moja {0}".format(text)
-
-
+            # else:
+            #     response = "END umechagua moja {0}".format(text)
 
             else:
 
